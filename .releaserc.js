1fdba5c3687729d6a03b2267aae51bebac3a9746
ldu1020 <ldu1020@github.com>
2021-02-26 15:06:53 +0900

    fix(add release): add

---
+++ .releaserc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  branches: [
+    { name: "main" },
+    { name: "develop", channel: "develop" },
+    { name: "add-release", channel: "add-release", prerelease: "Add-release" },
+  ],
+};
